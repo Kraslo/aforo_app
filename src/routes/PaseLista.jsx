968d1b43b10d609e5dbd6da53b9eab6493ef62aa
Ignacio Escribano Benavente <ignacio.escribano.ext@inetum.com>
2025-02-11 01:31:08 +0100

    Initial commit

--- src/routes/PaseLista.jsx
+++ src/routes/PaseLista.jsx
@@ -7,15 +7,17 @@ const PaseLista = () => {
 
     const [users, setUsers] = useState();
     const [isLoading, setIsLoading] = useState();
-    const { token } = useAuth();
+    const { token, logout } = useAuth();
 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const json_response = await submitlista(token.response.access_token, users); // users is supposed to be the formdata
         } catch (error) {
-            console.error(error);
+            if (error.message.includes("Token")) {
+                logout();
+            }
         };
     };
 
